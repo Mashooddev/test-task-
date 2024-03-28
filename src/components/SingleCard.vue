acc36309d1b505e14211a3895cde0511e821ac53
Mashooddev <127189299+Mashooddev@users.noreply.github.com>
2024-03-28 19:04:31 +0500

    Add files via upload

---
+++ src/components/SingleCard.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+      <figure class="w-full h-[250px] my-2">
+        <img :src="image" alt="image" class="rounded-md h-full w-full">
+      </figure>
+      <div class='flex items-start gap-2'>
+        <img :src="profileImage" alt="profileimage" class='h-10 w-10 object-cover rounded-full'/>
+        <div>
+          <h5 class='font-semibold text-lg'>{{ name }}</h5>
+          <p class="font-normal text-base">{{ title }}</p>
+          <p class="font-normal text-sm">{{ views }}</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      image: {
+        type: String,
+        default: ""
+      },
+      profileImage: {
+        type: String,
+        default: ""
+      },
+      name: {
+        type: String,
+        default: ""
+      },
+      title: {
+        type: String,
+        default: ""
+      },
+      views: {
+        type: Number,
+        default: 0
+      }
+    }
+  }
+  </script>
+  
---
+++ src/views/Home.vue
@@ -0,0 +1,54 @@
+
+<template>
+	<div class="w-full ">
+		<Header/>
+	<section class="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-4 gap-4 px-5 py-3">
+		<div v-for="({image,name,profileImage,title,views}) in singleCardData">
+			<SingleCard
+			 :image="image"
+			 :profileImage="profileImage"
+			 :title="title"
+			 :views="views"
+			 :name="name"
+			/>
+		</div>
+		
+	</section> 	
+	</div>
+</template>
+
+<script>
+import Header from '../components/Header.vue';
+import SingleCard from '../components/SingleCard.vue';
+export default {
+  components: {
+		Header,
+	SingleCard
+	},
+	data() {
+		const singleCardData = [
+			{title:"Arslan",name:"usman",profileImage:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",views:100,image:"https://cdn.quasar.dev/img/parallax1.jpg"
+			},
+			{title:"outfilters",name:"usman",profileImage:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",views:100,image:"https://cdn.quasar.dev/img/mountains.jpg"
+			},
+			{title:"outfilters",name:"usman",profileImage:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",views:100,image:"https://cdn.quasar.dev/img/parallax1.jpg"
+			},
+			{title:"single",name:"usman",profileImage:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",views:100,image:"https://cdn.quasar.dev/img/parallax1.jpg"
+			},
+			{title:"single",name:"usman",profileImage:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",views:100,image:"https://cdn.quasar.dev/img/parallax1.jpg"
+			},
+			{title:"single",name:"usman",profileImage:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",views:100,image:"https://cdn.quasar.dev/img/quasar.jpg"
+			},
+			{title:"single",name:"usman",profileImage:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",views:100,image:"https://cdn.quasar.dev/img/parallax1.jpg"
+			},
+			{title:"single",name:"usman",profileImage:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",views:100,image:"https://cdn.quasar.dev/img/parallax1.jpg"
+			},
+		// 	{title:"single",name:"usman",profileImage:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",views:100,image:"https://cdn.quasar.dev/img/quasar.jpg"
+		// },
+		]
+		return {
+		singleCardData
+	 }
+  }
+}
+</script>
